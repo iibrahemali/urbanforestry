a2651a0f778cb950cffbac0e08458926be6e82c9
Will Nguyen <mdnguyen123321@gmail.com>
2026-03-12 15:05:31 -0400

    added a comment

--- app/src/main/java/com/example/urbanforestry/MainActivity.java
+++ app/src/main/java/com/example/urbanforestry/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //This is Will
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
