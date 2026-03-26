4031ce040a462bc10e8e682883e62498cd88158d
WrenchBip <abailey@fandm.edu>
2026-03-25 21:07:24 -0400

    Added the overlay for the map, next step will be to add the dot tracking the location

--- app/src/main/java/com/example/urbanforestry/HomePage.java
+++ app/src/main/java/com/example/urbanforestry/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.urbanforestry;
 
+import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +11,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 
 public class HomePage extends AppCompatActivity {
+
+    private final int permRequestCode = 1;
+    private MapView map = null;
+    private MyLocationNewOverlay locationOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_home_page);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+
+        requestPermissions(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                }, permRequestCode);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        map.onResume(); // an overridden method so the map doesn't have to be destroyed and recreated
+        if(locationOverlay != null)
+            locationOverlay.enableMyLocation();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        map.onPause();
+        if(locationOverlay != null)
+            locationOverlay.disableMyLocation();
     }
 }
