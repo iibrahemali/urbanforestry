5e3119df5aff15c90580d36d017c60e4f5b2f3c9
John "Oley" Birkeland <oleybirkeland@gmail.com>
2026-04-07 15:38:02 -0400

    Replace CameraActivity with external camera app

--- app/src/main/java/com/example/urbanforestry/CreatePostActivity.java
+++ app/src/main/java/com/example/urbanforestry/CreatePostActivity.java
@@ -1,35 +1,62 @@
 package com.example.urbanforestry;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 public class CreatePostActivity extends AppCompatActivity {
-
-    private ActivityResultLauncher<Intent> launcher =
-            registerForActivityResult(
-                    new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        if (result.getResultCode() == RESULT_OK) {
-                            setResult(RESULT_OK, result.getData());
-                            finish(); // pass back to FeedActivity
-                        }
-                    });
+    private String photoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
 
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        setResult(RESULT_OK, result.getData());
+                        finish(); // pass back to FeedActivity
+                    }
+                });
+
+        ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent i = new Intent();
+                        i.putExtra("imagePath", photoPath);
+                        setResult(RESULT_OK, i);
+                        finish();
+                    }
+                });
+
         findViewById(R.id.btn_camera).setOnClickListener(v -> {
-            launcher.launch(new Intent(this, CameraActivity.class));
+            try {
+                String fileName = "pic_" + System.currentTimeMillis();
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                File photoFile = File.createTempFile(fileName, ".jpg", storageDir);
+
+                photoPath = photoFile.getAbsolutePath();
+                Uri photoUri = FileProvider.getUriForFile(this,
+                        getApplicationContext().getPackageName() + ".fileprovider", photoFile);
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                cameraLauncher.launch(cameraIntent);
+            } catch (Exception e) {
+                Log.d("URBAN FORESTRY", "Error creating file");
+            }
         });
 
         findViewById(R.id.btn_text).setOnClickListener(v -> {
